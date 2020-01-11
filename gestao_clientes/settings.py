d363bd9d5c3ace5bbee0c66173fcfa69e823afde
Yuri <contact@yurix.com>
2020-01-11 20:57:02 +0100

    Intro

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['gestao-clientes2.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['gestao-clientes2.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
--- home/views.py
+++ home/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 
-
+# TODO: Hacer seguimiento de solicitudes
 def home(request):
     return render(request, 'home.html')
 
-
+# FIXME: No funka nene
 def my_logout(request):
     logout(request)
     return redirect('home')
