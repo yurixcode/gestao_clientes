fffc16dd3b11529c052e153524455ac946a80c2b
Yuri <contact@yurix.com>
2020-01-12 18:26:11 +0100

    CCBV -View

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, my_logout
+from .views import home, my_logout, MyView
 
 
 urlpatterns = [
     path('', home, name="home"),
     path('logout/', my_logout, name="logout"),
+    path('view/', MyView.as_view(), name="view"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from django.http import HttpResponse
+from django.views import View
+
 
 # TODO: Hacer seguimiento de solicitudes
 def home(request):
     return render(request, 'home.html')
 
-# FIXME: No funka nene
 def my_logout(request):
     logout(request)
     return redirect('home')
+
+
+class MyView(View):
+    def get(self, request, *args, **kwargs):
+       return render(request, 'home3.html')
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponse('POST')
+
