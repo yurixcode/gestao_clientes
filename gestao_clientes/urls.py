70530ce0254be2692eecba752219cb5fe0d758ad
Yuri <contact@yurix.com>
2020-01-23 20:14:18 +0100

    Using Django Debug Toolbar

--- gestao_clientes/urls.py
+++ gestao_clientes/urls.py
@@ -1,25 +1,13 @@
-"""gestao_clientes URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# Django
 from django.contrib import admin
 from django.urls import path, include
-from clientes import urls as clientes_urls
-from home import urls as home_urls
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+
+# Views
+from clientes import urls as clientes_urls
+from home import urls as home_urls
 
 
 urlpatterns = [
@@ -29,3 +17,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+from django.urls import include, path  # For django versions from 2.0 and up
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+        
+    ] + urlpatterns
