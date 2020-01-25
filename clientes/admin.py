ef0abfb6a3f50b38b482fdeaa7618719d8b41a4e
Yuri <contact@yurix.com>
2020-01-25 21:31:34 +0100

    After customizing Django Admin

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 from .models import Person, Documento, Venda, Produto
 
+class PersonAdmin(admin.ModelAdmin):
 
-admin.site.register(Person)
+    fieldsets = (
+        ('Información básica', {
+        'fields' : ('doc', ('first_name', 'last_name'), ('age', 'salary'))
+        }),
+
+        ('Información avanzada', {
+            'classes' : ('collapse',),
+            'fields' : ('bio', 'photo')
+        }),
+    )
+
+    """En list_display señalamos los campos que queremos mostrar previo a acceder los datos"""
+    list_display = ('first_name', 'doc', 'last_name', 'age', 'salary', 'bio', 'photo')
+
+
+admin.site.register(Person, PersonAdmin)
 admin.site.register(Documento)
 admin.site.register(Venda)
 admin.site.register(Produto)
