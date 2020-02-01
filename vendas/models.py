f2cfbfd9d5c5844fdd643d37968200da6a806d32
Yuri <contact@yurix.com>
2020-02-01 20:50:47 +0100

    Practique with the django orm

--- vendas/models.py
+++ vendas/models.py
@@ -1,8 +1,9 @@
 # Django
 from django.db import models
+from django.db.models import Sum, F, FloatField, Max
 
 # Signals
-from django.db.models.signals import m2m_changed
+from django.db.models.signals import post_save, post_delete, m2m_changed
 from django.dispatch import receiver
 
 # Models
@@ -14,22 +15,31 @@ class Venda(models.Model):
     numero = models.CharField(max_length=7)
     valor = models.DecimalField(max_digits=5, decimal_places=2, default=0)
     desconto = models.DecimalField(max_digits=5, decimal_places=2, default=0)
-    impostos = models.DecimalField(max_digits=5, decimal_places=2)
+    impostos = models.DecimalField(max_digits=5, decimal_places=2, default=0)
     pessoa = models.ForeignKey(Person, null=True, blank=True, on_delete=models.PROTECT)
+    produto = models.ManyToManyField(Produto, through='ItemDoPedido')
     nfe_emitida = models.BooleanField()
     
     def __str__(self):
         return self.numero
 
-    # def get_total(self):
-    #     total = 0
-    #     for produto in self.produtos.all():
-    #         total += produto.preco
+    def update_totals(self):
+        """Solicitamos un nuevo cálculo de los totales para 
+        posteriormente actualizar la bd"""
+        self.get_total()
 
-    #     self.valor = (total - self.desconto) + self.impostos
-    #     self.save()
 
-    #     return self.valor
+    def get_total(self):
+        """Calculamos el total de todos los productos, con sus cantidades,
+        descuentos por producto, descuento por la compra general y además,
+        incluimos los impuestos..."""
+
+        tot = self.itemdopedido_set.all().aggregate(
+            tot_ped = Sum((F('quantidade') * F('produto__preco')) - F('desconto'), output_field=FloatField())
+        )['tot_ped'] or 0
+
+        tot = (tot + float(self.impostos)) - float(self.desconto)
+        Venda.objects.filter(id=self.id).update(valor=tot)
 
 
 class ItemDoPedido(models.Model):
@@ -43,8 +53,21 @@ class ItemDoPedido(models.Model):
         return f'{self.venda} - {self.produto}({self.quantidade})'
 
 
-# @receiver(m2m_changed, sender=Venda.produtos.through)
-def update_totals(sender, instance, *args, **kwargs):
+@receiver(post_save, sender=ItemDoPedido)
+def update_vendas_total(sender, instance, *args, **kwargs):
+    instance.venda.get_total()
+
+@receiver(post_save, sender=Venda)
+def update_vendas_total2(sender, instance, *args, **kwargs):
     instance.get_total()
 
-# m2m_changed.connect(update_totals, sender=Venda.produtos.through)
+@receiver(post_delete, sender=ItemDoPedido)
+def update_vendas_total3(sender, instance, *args, **kwargs):
+    instance.venda.get_total()
+
+
+# @receiver(m2m_changed, sender=Venda.produto.through)
+# def update_vendas_total2(sender, instance, action, *args, **kwargs):
+#     if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
+#         instance.get_total()
+
